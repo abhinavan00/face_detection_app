a0fa6287a656b0619cc12f5832b384454a57f24a
Abhinav Anand <abhinavanand@Mac.lan>
2025-05-21 20:15:38 +0530

    updated vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  
+  server: {
+    proxy: {
+      // Add this if you want to proxy API requests during development
+      '/api': {
+        target: 'https://face-detection-app-api-fb7t.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
