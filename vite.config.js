baf6da59db0e767fd353890884f4dff44096019e
Abhinav Anand <abhinavanand@Mac.lan>
2025-05-21 23:20:22 +0530

    updated register.jsx and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  server: {
-    proxy: {
-      // Add this if you want to proxy API requests during development
-      '/api': {
-        target: 'https://face-detection-app-api-fb7t.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
